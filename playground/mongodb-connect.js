b4f9ab7ef269123259d9e6cc3cd652a2d7e88871
Justin Joy <JJ@Dev-Team.Dlink>
2018-07-03 22:24:11 +0530

    MongoDB Document find code added

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser:true}, (err, client) => {
@@ -8,21 +9,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser:true},
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').insertOne({
-        _id: 123,
-        text: 'Code Code Code',
-        completed: false
-    }, (err, res) => {
-        if (err) {
-            return console.log('Unable to insert todo', err);
-        }
-        console.log(JSON.stringify(res.ops, undefined, 2));
-    });
+    // db.collection('Todos').insertOne({
+    //     text: 'Code Code Code',
+    //     completed: false
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(res.ops[0]._id.getTimestamp());
+    // });
 
     // db.collection('Users').insertOne({
-    //      name: 'Justin',
-    //      age: 26,
-    //      location: 'Bangalore'
+    //      name: 'Avinash',
+    //      age: 25,
+    //      location: 'Himachal'
     // }, (err, res) => {
     //     if (err) {
     //         return console.log('Unable to insert user', err);
