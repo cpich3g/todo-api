823e7089fda5787ca3209e803339ec75ab655bdb
Justin Joy <JJ@Dev-Team.Dlink>
2018-07-29 13:48:41 +0530

    express code added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on Port 3000');
+});
+
+
+
 
 // var newTodo = new Todo({
 //     text: 'Cook Dinner'
@@ -22,13 +48,13 @@ var {User} = require('./models/user');
 //     console.log('Unable to save', e);
 // });
 
-var newUser = new User({
-    user: 'Justin',
-    email: '  justin@njs.com  '
-});
+// var newUser = new User({
+//     user: 'Justin',
+//     email: '  justin@njs.com  '
+// });
 
-newUser.save().then((docs) => {
-    console.log(JSON.stringify(docs, undefined, 2));
-}, (e) => {
-    console.log(e);
-});
+// newUser.save().then((docs) => {
+//     console.log(JSON.stringify(docs, undefined, 2));
+// }, (e) => {
+//     console.log(e);
+// });
