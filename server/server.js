61d52f98942dcd897dfe799c455683e61f84476f
Justin Joy <JJ@Dev-Team.Dlink>
2018-07-29 17:16:29 +0530

    dev dependencies added for testing

--- server/server.js
+++ server/server.js
@@ -25,36 +25,3 @@ app.listen(3000, () => {
     console.log('Started on Port 3000');
 });
 
-
-
-
-// var newTodo = new Todo({
-//     text: 'Cook Dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save todo')
-// });
-
-// var inTodo = new Todo({
-//     text: '  Edit this  '
-// });
-
-// inTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save', e);
-// });
-
-// var newUser = new User({
-//     user: 'Justin',
-//     email: '  justin@njs.com  '
-// });
-
-// newUser.save().then((docs) => {
-//     console.log(JSON.stringify(docs, undefined, 2));
-// }, (e) => {
-//     console.log(e);
-// });
